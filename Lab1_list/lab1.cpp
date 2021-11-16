f5038a421d6b2f38dfb7e021b446300c5318b675
VetaSolo <74370323+VetaSolo@users.noreply.github.com>
2021-11-16 18:12:17 +0300

    Add files via upload

---
+++ Lab1_list/lab1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "MyList.h"
+using namespace std;
+
+int main()
+{
+	MyList list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	cout << list << endl;
+	cout << list.get_size()<<" - size" << endl;
+	list.reverse();
+	cout << list << " - reverse" << endl;
+
+	list.push_front(2384);
+	list.remove(2);
+	cout << list << " - remove(2)" << endl;
+	list.clear();
+	cout << list << " - clear" << endl;;
+
+	return 0;
+}
